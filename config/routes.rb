73084f0639c4c93fc0dbf3fae632c87a43ba0f38
yago santos <yagolcs@msn.com>
2020-02-20 14:50:59 -0300

    Create models and migratation for Cocktail, Dose and Ingredient.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: %i[index show new create]
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'json'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +10,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding started...."
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+json = JSON.parse(response.read)
+json["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+puts "Seeding complete!"
