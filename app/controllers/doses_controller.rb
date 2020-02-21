dfc844a0e8b425c5a57b142c31aa0b0eb6ecaedc
yago santos <yagolcs@msn.com>
2020-02-21 09:20:01 -0300

    Modify cocktails and doses controllers. Implement dose form in cocktail show view.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,26 @@
 class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: %i[index show new create]
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[create]
+  end
+  resources :doses, only: %i[destroy]
   root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
